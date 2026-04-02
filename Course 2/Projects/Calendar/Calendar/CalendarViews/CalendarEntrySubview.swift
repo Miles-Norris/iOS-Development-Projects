bed6fe26a4b8045f7b20ab363966076e8c554b16
Miles Norris <norrismilesb@gmail.com>
2026-04-02 10:11:04 -0600

    Started the main calendar view

--- Course 2/Projects/Calendar/Calendar/CalendarViews/CalendarEntrySubview.swift
+++ Course 2/Projects/Calendar/Calendar/CalendarViews/CalendarEntrySubview.swift
@@ -8,11 +8,47 @@
 import SwiftUI
 
 struct CalendarEntrySubview: View {
+    let calendarEntry: CalendarEntry
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            VStack(alignment: .center, spacing: 0) {
+                Text(calendarEntry.date.suffix(2))
+                    .font(.system(size: 45))
+                    .bold()
+                Text(calendarEntry.date.prefix(3))
+                    .font(.system(size: 20))
+                    .bold()
+            }
+            
+            Divider()
+                .frame(width: 20, height: 75)
+            
+            Spacer()
+            
+            VStack(alignment: .center) {
+                Text(calendarEntry.lessonID)
+                    .font(.system(size: 15))
+                    .bold()
+                
+                Text(calendarEntry.lessonName)
+                    .font(.system(size: 20))
+                    .bold()
+            }
+            
+            Spacer()
+        }
+        .padding()
+        .background {
+            RoundedRectangle(cornerRadius: 15)
+                .foregroundStyle(.white)
+                .shadow(radius: 10)
+        }
+        .frame(minWidth: 365)
+        .frame(maxWidth: 365)
+        .padding(.bottom, 10)
     }
 }
 
 #Preview {
-    CalendarEntrySubview()
+    CalendarEntrySubview(calendarEntry: CalendarEntry.calendarEntrys[2])
 }
--- Course 2/Projects/Calendar/Calendar/CalendarViews/CalendarView.swift
+++ Course 2/Projects/Calendar/Calendar/CalendarViews/CalendarView.swift
@@ -8,8 +8,30 @@
 import SwiftUI
 
 struct CalendarView: View {
+    @State var swiftFundamentals = CalendarEntry.calendarEntrys.filter { $0.lessonID.hasPrefix("SF") }
+    @State var tablesAndPersistence = CalendarEntry.calendarEntrys.filter { $0.lessonID.hasPrefix("TP") }
+    @State var networkingAndDataStorage = CalendarEntry.calendarEntrys.filter { $0.lessonID.hasPrefix("ND") }
+    @State var specialTopics = CalendarEntry.calendarEntrys.filter { $0.lessonID.hasPrefix("ST") }
+    @State var fullAppDevelopment = CalendarEntry.calendarEntrys.filter { $0.lessonID.hasPrefix("FA") }
+    @State var prototypeAndProjectPlanning = CalendarEntry.calendarEntrys.filter { $0.lessonID.hasPrefix("PC") }
+    @State var groupCapstone = CalendarEntry.calendarEntrys.filter { $0.lessonID.hasPrefix("GC") }
     var body: some View {
-        Text("Hello, World!")
+        NavigationStack {
+            ScrollView {
+                VStack(alignment: .leading) {
+                    if !swiftFundamentals.isEmpty {
+                        Text("Swift Fundamentals")
+                            .font(.system(size: 35))
+                            .bold()
+                        
+                        ForEach(swiftFundamentals) { lesson in
+                            CalendarEntrySubview(calendarEntry: lesson)
+                        }
+                    }
+                }
+                .frame(maxWidth: .infinity)
+            }
+        }
     }
 }
 
